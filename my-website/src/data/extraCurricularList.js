7166986a4d2b2e6fc2f33e29c7f25eecaef45d9d
Spencer Dobrik <dobrik.justskills@gmail.com>
2019-02-10 15:42:29 -0500

    Added descriptions for ECs
    Also added links

--- my-website/src/data/extraCurricularList.js
+++ my-website/src/data/extraCurricularList.js
@@ -1,50 +1,71 @@
 export default [
     {
+        activityName: 'Varsity Men\'s Basketball',
+        timespan: {
+            startDate: 'July 2015',
+            endDate: 'September 2016'
+        },
+        location: 'University of Waterloo',
+        position: 'Forward/Centre',
+        description: '',
+        bulletPoints: [
+            'Developed excellent time management, balancing a full course load with over 25 hours of basketball commitment per week.',
+            'Cultivated teamwork and leadership skills while building a strong work ethic.',
+            'Pretty good at pick-up now.'
+        ],
+        logo: '',
+        link: 'https://athletics.uwaterloo.ca/roster.aspx?roster=159&path=mbball',
+        key: 'activity0'
+    },{
         activityName: 'FEDS Student Council',
         timespan: {
-            startDate: '',
-            endDate: ''
+            startDate: 'May 2016',
+            endDate: 'April 2017'
         },
         location: 'University of Waterloo',
         position: 'Engineering Councillor',
         description: '',
         bulletPoints: [
-            ''
+            'Helped shape the policies around the university and bring a voice to my constituents.',
+            'Member of co-op students council, which works with CECA to improve co-op at University of Waterloo.',
+            'Member of the student life endowment fund committee.'
         ],
         logo: '',
-        link: '',
+        link: 'https://feds.ca/',
         key: 'activity1'
     },
     {
         activityName: 'Watonomous',
         timespan: {
-            startDate: '',
-            endDate: ''
+            startDate: 'September 2017',
+            endDate: 'December 2017'
         },
         location: 'University of Waterloo',
-        position: 'Engineering Councillor',
+        position: 'User Interface Team Member',
         description: '',
         bulletPoints: [
-            ''
+            'Designed and implemented an interface to help other teams visualize data and communicate with occupants.',
+            'Developed the UI for the autonomous car using ROS and Python.'
         ],
         logo: '',
-        link: '',
+        link: 'https://watonomous.ca/#/',
         key: 'activity2'
     },
     {
-        activityName: 'EngSoc Rep',
+        activityName: 'EngSoc Student Council',
         timespan: {
-            startDate: '',
-            endDate: ''
+            startDate: 'January 2018',
+            endDate: 'Current'
         },
         location: 'University of Waterloo',
-        position: '',
+        position: 'Software Engineering Representative',
         description: '',
         bulletPoints: [
-            ''
+            'Represented my Software Engineering class.',
+            'Advocated for the acceptance of the SE Society as a related society.'
         ],
         logo: '',
-        link: '',
+        link: 'https://www.engsoc.uwaterloo.ca/',
         key: 'activity3'
     },
     {
@@ -57,11 +78,14 @@ export default [
         position: 'Founder and A-Soc President',
         description: '',
         bulletPoints: [
-            '',
-            ''
+            'Worked with Faculty, Students, and other Societies in order to found the society.',
+            'Recruited students to help found and run the first iteration of the society.',
+            'Organized meeting, as well as the tasks and responsibilities for the representatives of the society.',
+            'Created a Constitution which was ratified with 94% of votes in favour, and a voter turnout of 42.5%.',
+            'Ran three events which were attended by ~50% of all Software Engineering students in school that term.'
         ],
         logo: '',
-        link: '',
+        link: 'https://www.facebook.com/uwsesoc/',
         key: 'activity4'
     }
 ]
